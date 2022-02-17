2053ef9dc213aab52d8cd67a6b1ec61116b55d9a
math-yew <math.u@hotmail.com>
2022-02-17 14:43:06 -0700

    Object replace

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("script");
 
-const a = [
+const a = {w:14,h;30,pics:[
   "art1.jpg",
   "art2.jpg",
   "art3.jpg",
@@ -16,15 +16,39 @@ const a = [
   "sequoia.jpg",
   "table.jpg",
   "temple.jpg"
-]
+]}
+
+
+const b = {w:14,h;30,pics:[
+  "art1.jpg",
+  "art2.jpg",
+  "art3.jpg
+}
+
+
+const c = {w:14,h;30,pics:[
+  "art1.jpg",
+  "art2.jpg",
+  "art3.jpg
+}
+
+
+const d = {w:14,h;30,pics:[
+  "art1.jpg",
+  "art2.jpg",
+  "art3.jpg
+}
 
 let mainDiv = document.getElementById("main");
 
-function populate(imageArr){
+function populate(obj){
+let imageArr = obj.pics;
 for (var i = 0; i < imageArr.length; i++) {
   let div = document.createElement("DIV");
   div.setAttribute("class", "gallery");
   div.style.backgroundImage = "url('images/"+ imageArr[i] +"')";
+  div.style.width = obj.w;
+  div.style.height = obj.h;
   document.getElementById("main").appendChild(div);
 }
 }
